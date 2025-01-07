ad14d62fe0d17530c40f46c75364f1b3032cc7bd
Walter Ruganzu <rwalter692@gmail.com>
2025-01-07 12:45:23 +0300

    dynamic urls

--- tasks/urls.py
+++ tasks/urls.py
@@ -8,29 +8,32 @@ from rest_framework_simplejwt.views import (
 )
 
 def home(request):
-    # HTML response with refined styling
-    html_content = """
+    # Dynamically determine the host
+    current_host = request.get_host()
+
+    # HTML response with refined styling and dynamic host handling
+    html_content = f"""
     <html>
         <head>
             <title>Task Manager API</title>
             <style>
-                body {
+                body {{
                     font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
                     text-align: center;
                     margin-top: 100px;
                     background-color: #f4f7f6;
-                }
-                h1 {
+                }}
+                h1 {{
                     font-size: 60px;
                     color: #333;
                     margin-bottom: 20px;
                     font-weight: bold;
-                }
-                p {
+                }}
+                p {{
                     font-size: 20px;
                     color: #555;
-                }
-                a {
+                }}
+                a {{
                     font-size: 25px;
                     color: black;
                     text-decoration: none;
@@ -39,32 +42,31 @@ def home(request):
                     border-radius: 8px;
                     border: 1px solid #ddd;
                     transition: background-color 0.3s, color 0.3s;
-                }
-                a:hover {
+                }}
+                a:hover {{
                     background-color: #007BFF;
                     color: white;
-                }
-                .container {
+                }}
+                .container {{
                     max-width: 600px;
                     margin: 0 auto;
                     padding: 20px;
                     background-color: #ffffff;
                     border-radius: 12px;
                     box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
-                }
+                }}
             </style>
         </head>
         <body>
             <div class="container">
                 <h1>Task Manager API</h1>
                 <p>Welcome to the Task Manager API! You can manage tasks and users through the API.</p>
-                <p><a href="http://localhost:8000/swagger/">Documentation</a></p>
+                <p><a href="http://{current_host}/swagger/">Documentation</a></p>
             </div>
         </body>
     </html>
     """
     return HttpResponse(html_content)
-
 urlpatterns = [
     path('', home, name='home'),  # Add this line for the root URL
     path('register/', RegisterUserView.as_view(), name='register_user'),
